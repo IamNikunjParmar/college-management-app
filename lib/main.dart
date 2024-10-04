4ff298ea82c819ce64a5ce33928267bd1e876caa
IamNikunjParmar <nikunjparmar5066@gmail.com>
2024-10-04 12:41:18 +0530

    creating register screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:college_management_app/src/app/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      routes: AppRoutes.myRoutes,
+    );
+  }
+}
